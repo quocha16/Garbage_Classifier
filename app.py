1d82e5ec16d534ae7117926c88e790a97e125731
quocha16 <anthonyquoc168@gmail.com>
2026-01-11 11:26:41 +0700

    Upload

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ import joblib
 import numpy as np
 import cv2
 import os
+from sklearn.preprocessing import normalize
 
 app = Flask(__name__)
 
-# CẤU HÌNH
-WIDTH = 96
-HEIGHT = 96
+WIDTH, HEIGHT = 128, 128
 IMG_SIZE = (WIDTH, HEIGHT)
 VOCAB_SIZE = 800
 
@@ -16,57 +15,42 @@ VOCAB_SIZE = 800
 base_path = "model/"
 
 try:
-    svm_model = joblib.load(os.path.join(base_path, "svm_garbage_model.pkl"))
+    dnn_model = joblib.load(os.path.join(base_path, "dnn_garbage_model.pkl"))
     pca_model = joblib.load(os.path.join(base_path, "pca_garbage_model.pkl"))
     le_model = joblib.load(os.path.join(base_path, "le_garbage_model.pkl"))
-    kmeans_model = joblib.load(os.path.join(base_path, "kmeans_garbage_model.pkl"))
+    gmm_model = joblib.load(os.path.join(base_path, "gmm_garbage_model.pkl"))
     scaler_model = joblib.load(os.path.join(base_path, "scaler_garbage_model.pkl"))
-except Exception:
-    pass
+except Exception as e:
+    print(f"Error: {e}")
 
-orb = cv2.ORB_create(nfeatures=500)
+orb = cv2.ORB_create(nfeatures=800)
 
 
-def extract_features_bow(img):
+def extract_features_combined(img):
     img_resized = cv2.resize(img, IMG_SIZE)
     gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
     kp, des = orb.detectAndCompute(gray, None)
 
+    #GMM Bag of Visual Words
     bow_hist = np.zeros(VOCAB_SIZE, dtype=np.float32)
-
     if des is not None:
-        preds = kmeans_model.predict(des.astype(float))
-        unique, counts = np.unique(preds, return_counts=True)
-        for u, c in zip(unique, counts):
-            if u < VOCAB_SIZE:
-                bow_hist[u] = c
+        probs = gmm_model.predict_proba(des.astype(np.float32))
+        bow_hist = probs.sum(axis=0)
 
-    bow_hist = cv2.normalize(bow_hist.reshape(1, -1), None, norm_type=cv2.NORM_L2).flatten()
+    #BoW
+    if bow_hist.sum() > 0:
+        bow_hist = normalize(bow_hist.reshape(1, -1), norm='l2').flatten()
 
+    #Color Histogram
     hsv = cv2.cvtColor(img_resized, cv2.COLOR_BGR2HSV)
     h_hist = cv2.calcHist([hsv], [0], None, [16], [0, 180]).flatten()
     s_hist = cv2.calcHist([hsv], [1], None, [16], [0, 256]).flatten()
     v_hist = cv2.calcHist([hsv], [2], None, [16], [0, 256]).flatten()
 
     color_feat = np.concatenate([h_hist, s_hist, v_hist])
-    color_feat = cv2.normalize(color_feat.reshape(1, -1), None, norm_type=cv2.NORM_L2).flatten()
+    color_feat = normalize(color_feat.reshape(1, -1), norm='l2').flatten()
 
     return np.concatenate([bow_hist, color_feat])
-
-
-def preprocess_image(file_storage):
-    file_bytes = np.frombuffer(file_storage.read(), np.uint8)
-    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-
-    if img is None:
-        raise ValueError("Lỗi đọc ảnh")
-
-    feat = extract_features_bow(img)
-    feat = feat.reshape(1, -1)
-    feat_scaled = scaler_model.transform(feat)
-
-    feat_pca = pca_model.transform(feat_scaled)
-    return feat_pca
 
 
 @app.route("/")
@@ -79,19 +63,43 @@ def predict():
     try:
         if "file" not in request.files:
             return jsonify({"error": "No file part"})
+
         file = request.files["file"]
         if file.filename == '':
             return jsonify({"error": "No selected file"})
 
-        X_final = preprocess_image(file)
-        y_pred = svm_model.predict(X_final)
-        class_name = le_model.inverse_transform(y_pred)[0]
+        #Đọc và tiền xử lý ảnh
+        file_bytes = np.frombuffer(file.read(), np.uint8)
+        img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        if img is None:
+            return jsonify({"error": "Lỗi đọc ảnh"})
 
-        return jsonify({"prediction": class_name})
+        #Trích xuất đặc trưng
+        feat = extract_features_combined(img).reshape(1, -1)
+        feat_scaled = scaler_model.transform(feat)
+        feat_pca = pca_model.transform(feat_scaled)
+
+        probs = dnn_model.predict_proba(feat_pca)[0]
+
+        top_4_idx = np.argsort(probs)[-4:][::-1]
+
+        top_4_list = []
+
+        for idx in top_4_idx:
+            top_4_list.append({
+                "label": le_model.inverse_transform([idx])[0],
+                "confidence": round(float(probs[idx]) * 100, 2)
+            })
+
+
+        return jsonify({
+            "prediction": top_4_list[0]["label"],
+            "top_4": top_4_list
+        })
 
     except Exception as e:
         return jsonify({"error": str(e)})
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
